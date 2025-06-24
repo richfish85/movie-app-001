e82edeb2fb08844a6fc9c628b15ec81dd9ea81ad
Richard Fisher <39341551+richfish85@users.noreply.github.com>
2025-06-25 00:43:49 +1000

    Add search type dropdown and popular listings

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React from 'react';
 
-const Search = ({ searchTerm , setSearchTerm }) => {
+const Search = ({ searchTerm, setSearchTerm, searchType, setSearchType }) => {
   return (
     <div className="search">
-        <div>
-            <img src="search.svg" alt="search" />
-            <input
-                type="text"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-                placeholder="Search through thousands of movies..."
-            />
+      <div>
+        <img src="search.svg" alt="search" />
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search through thousands of movies..."
+        />
+        <select
+          value={searchType}
+          onChange={(e) => setSearchType(e.target.value)}
+          className="ml-2 bg-dark-100 border border-gray-100 rounded-md text-gray-200 py-2 px-2"
+        >
+          <option value="movie">Movie</option>
+          <option value="tv">TV</option>
+        </select>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
